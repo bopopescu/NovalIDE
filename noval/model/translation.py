dd370050c49c5003631ced28fd8671d80b7d6bcf
wukan <wekay102200@sohu.com>
2018-12-23 23:19:28 +0800

    初步支持加载插件扩展

---
+++ noval/model/translation.py
@@ -0,0 +1,21 @@
+import wx
+from noval.tool.consts import _
+'''
+    this file is used to translation
+'''
+
+_("Fonts and Colors")
+_("Python Interpreter")
+_("Extension")
+_("Search Results")
+_("Outline")
+_("Projects/Resources View")
+_("Configuration List")
+
+_("Resource")
+_("Debug/Run")
+_("Project References")
+
+_("Yes")
+_("No")
+_("Logs")
